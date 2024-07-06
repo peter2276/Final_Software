5de7a17d4afa6aa8f7f4a1e54f566a35585ba476
peter2276 <pdelacruz@INVAP.COM.AR>
2024-07-06 18:48:31 -0300

    Algunas cosas de systemManager

--- systemmanager.h
+++ systemmanager.h
@@ -1,10 +1,22 @@
 #ifndef SYSTEMMANAGER_H
 #define SYSTEMMANAGER_H
+#include "alarm.h"
+#include "controller.h"
+#include "light.h"
+#include "heater.h"
 
 class SystemManager
 {
 public:
+    static SystemManager* getInstance();
+    Alarm * MyAlarm;
+    Heater * MyHeater;
+    std::list<Light> lights;
+
+protected:
     SystemManager();
+private:
+    static SystemManager* instance;
 };
 
 #endif // SYSTEMMANAGER_H
