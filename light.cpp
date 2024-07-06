cfbe3353a5a6b413ea8f36e6d8927fd9d422caff
unknown <francodalfarra@gmail.com>
2024-07-06 20:49:12 -0300

    Comienzo con clase lights

--- light.cpp
+++ light.cpp
@@ -1,3 +1,27 @@
 #include "light.h"
 
-Light::Light() {}
+Light::Light(){
+    state = 0;
+}
+
+void Light::setActuatorOn(Port port){
+    if (state == 0){
+        this->board->digitalWrite(port,1);
+        this->state = 1;
+    }
+}
+
+void Light::setActuatorOff(Port port){
+    if (state == 1){
+        this->board->digitalWrite(port,0);
+        this->state = 0;
+    }
+}
+
+void Light::setController(Controller *board){
+    this->board = board;
+}
+
+bool Light::getState(){
+    return this->state;
+}
--- light.h
+++ light.h
@@ -1,12 +1,20 @@
 #ifndef Light_H
 #define Light_H
 
+#include "controller.h"
+
 class Light
 {
 public:
     Light();
+    void setActuatorOn(Port port);
+    void setActuatorOff(Port port);
+    void setController(Controller *board);
+    bool getState();
 private:
-
+    Controller* board;
+    Port actuator;
+    bool state;
 };
 
 #endif // Light_H
